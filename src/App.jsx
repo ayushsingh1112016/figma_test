aac0b05a05be590f5a3b756c7b741600e9f0fd22
ayushsingh1112016 <ayushsingh1112016@gmail.com>
2024-11-15 14:58:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './assets/Components/Header/Header';
-import Home from './assets/Components/Home/Home'
+import Home from './assets/Components/Home/Home';
 import Footer from './assets/Components/Footer/Footer';
-
 import ContactUsPage from './assets/Components/ContactUsPage/ContactUsPage';
-import './App.css'
-function App() {
+import './App.css';
 
+function App() {
   return (
-    <>
-      <Header/>
-      <Home/>
-      {/* <ContactUsPage/> */}
-      <Footer/>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUsPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom'; // Import Link
 import logo from "./hdr_logo_image.png";
-import location from "./hdr_location.svg"
+import location from "./hdr_location.svg";
+
 const Header = () => {
   return (
     <header className="bg-white shadow-md py-4 px-10">
       <div className="container mx-auto flex items-center justify-between px-4">
-        {/* Logo Section */}
+        {/* Logo Section as Home Link */}
         <div className="flex items-center p-0 ">
-          <a href="#">
+          <Link to="/"> {/* Logo now acts as Home button */}
             <img src={logo} alt="AAJ Swift Logo" className="h-8" />
-          </a>
+          </Link>
         </div>
 
         {/* Navigation Links */}
         <nav className="hidden md:flex space-x-8">
+          <Link to="/" className="text-gray-700 hover:text-customRed">Home</Link> {/* Link to Home page */}
           <a href="#" className="text-gray-700 hover:text-customRed">Company</a>
           <a href="#" className="text-gray-700 hover:text-customRed">Calculate Shipping Cost</a>
           <a href="#" className="text-gray-700 hover:text-customRed">Blogs</a>
-          <a href="#" className="text-gray-700 hover:text-customRed">Contact Us</a>
+          <Link to="/contact" className="text-gray-700 hover:text-customRed">Contact Us</Link> {/* Link to Contact Us page */}
         </nav>
 
         {/* Right Section (Track Shipment and Login Button) */}
@@ -25,14 +28,12 @@ const Header = () => {
           <a href="#" className="text-customRed flex items-center space-x-1">
             {/* <i className="fas fa-map-marker-alt"></i> */}
             <span className="underline">Track Shipment</span>
-            <img src = {location}/>
+            <img src={location} alt="Location icon" />
           </a>
           <a href="#" className="bg-customRed text-white py-2 px-10 rounded-md hover:bg-red-800">
             Login
           </a>
         </div>
-
-        
       </div>
     </header>
   );
